8b66a58e7da5a88965ca280491ae16b69102a42c
School <waughc@myumanitoba.ca>
2021-12-21 08:45:58 -0600

    Setup of project template.
    3 features, no formating. Very little code.

---
+++ app/src/main/java/personal/project/beercanoutlook/ChangeSentenceActivity.java
@@ -0,0 +1,16 @@
+package personal.project.beercanoutlook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import personal.project.beercanoutlook.R;
+
+public class ChangeSentenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_sentence);
+    }
+}
